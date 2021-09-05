17df509d73045c266fd0ce2bc4870a77499a9007
Javier <jvrjsanchez@gmail.com>
2021-09-05 00:04:33 -0300

    Challenge 05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,7 +2,7 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myarray = [ '2swamped4u', 5, true, null, undefined];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
@@ -12,7 +12,8 @@ Crie uma função que receba um array como parâmetro, e retorne esse array.
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log ( myarray );
+
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
