8ec205096248523a6491bfe160370335a7d35094
Javier <jvrjsanchez@gmail.com>
2021-09-20 09:30:41 -0300

    Challenge-07

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -1,7 +1,7 @@
 /*
 Crie um array com 5 items (tipos variados).
 */
-// ?
+var array = ['steelers', 2.5, true, null, undefined];
 
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
