e44be746d53e922a2cfca1fe8d33339d1a9fb0af
2swamped4u <jvrjsanchez@gmail.com>
2021-10-21 15:27:54 -0300

    Challenge 09

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -3,7 +3,10 @@ Crie uma IIFE que envolva todo o código desse arquivo. Faça também a
 indentação correta do código, para ficar dentro da IIFE.
 */
 
-function(){}
+function(){
+
+
+}
 
 /*
 Analise as funções abaixo (`myFunction`, `myFunction2` e `myFunction3`, e
