db1d4611556de7be2295243f1620114db630ce44
2swamped4u <jvrjsanchez@gmail.com>
2021-11-03 12:56:04 -0400

    example javascript file

--- ninja.js
+++ ninja.js
@@ -1,11 +1,11 @@
 function myFunction() {
 
-	var number1 = 1;
-	var number2 = 2;
-	return sum();
 	function sum() {
 		return number1 + number2;
 	}
+	var number1 = 1;
+	var number2 = 2;
+	return sum();
 }
 
 console.log( myFunction() );
