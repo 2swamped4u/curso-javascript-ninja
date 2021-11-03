fa82742180ad81c6aae4cb42413b25a7c4662108
2swamped4u <jvrjsanchez@gmail.com>
2021-11-03 13:27:58 -0400

    example ninja practice

--- ninja.js
+++ ninja.js
@@ -1,4 +1,3 @@
-
 /*function myFunction() {
 	var number1 = 1;
 	var number2 = 2;
@@ -12,15 +11,29 @@
 }*/
 
 //----------
-function myFunction() {
 
-	var number1 = 1;
-	var number2 = 2;
-	return sum();
+/*function myFunction() {
 	function sum() {
-		return number1 + number2;
+		return 1 + 2;
 	}
+	console.log(sum());
+	console.log(sum());
 }
 
 console.log( myFunction() );
+*/
 
+function myFunction() {
+	var number1 = 10;
+	var number2 = 10;
+	function sum() {
+	return number1 + number2;
+
+	}
+	if( sum() === 20){
+		return 'equal to 20';
+	}
+	return 'no change ';
+}
+
+console.log( myFunction() );
