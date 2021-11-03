1fee72ed9115202f55dc93028d52bb0f6a0d2222
2swamped4u <jvrjsanchez@gmail.com>
2021-11-03 13:04:27 -0400

    example js

--- ninja.js
+++ ninja.js
@@ -1,11 +1,25 @@
+
+/*function myFunction() {
+	var number1 = 1;
+	var number2 = 2;
+	var sum = undefined;
+	return sum();
+	sum = function sum() {
+
+		...
+	}
+
+}*/
+
+//----------
 function myFunction() {
 
-	function sum() {
-		return number1 + number2;
-	}
 	var number1 = 1;
 	var number2 = 2;
 	return sum();
+	function sum() {
+		return number1 + number2;
+	}
 }
 
 console.log( myFunction() );
