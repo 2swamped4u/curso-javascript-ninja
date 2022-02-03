b18f5bb9e2d399c14290398f26f68685a7a63031
2swamped4u <jvrjsanchez@gmail.com>
2022-02-03 10:45:22 -0300

    Challenge 11

--- challenge-11/challenge-11.js
+++ challenge-11/challenge-11.js
@@ -58,9 +58,9 @@ Após a função, mostrar a mensagem no console:
 */
 
 function moreThan(age) {
-	return person.age > 25 ? true : false;
+	return person.age > age;
 	}
-console.log('The person has more than 25 years old?', moreThan(25));
+console.log('The person has more than 25 years old?', moreThan(35));
 
 /*
 Faça um loop de 0 a 20, que adicione cada número como um item de um
